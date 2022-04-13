2cfe76a226e099b8b7e0fa0e6361df07c2a89d44
daniKubatka <daniela.kubatka@gmail.com>
2022-04-13 20:13:22 +0200

    zelva nakresli domecek

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -4,11 +4,35 @@ import net.sevecek.turtle.engine.*;
 
 public class HlavniProgram {
 
-    public void main(String[] args) {
-        Turtle zofka;
+    private Turtle zofka;
 
+    public void main(String[] args) {
         zofka = new Turtle();
-        zofka.setLocation(50,150);
+
+        udelejDomecek();
     }
+
+    public void udelejCtverec(){
+        for (int i = 0; i < 4; i++) {
+            zofka.turnRight(90);
+            zofka.move(100);
+        }
+
+    }
+
+    public void udelejTrojuhelnik(){
+        zofka.turnRight(45);
+        zofka.move(72);
+        zofka.turnRight(90);
+        zofka.move(72);
+        zofka.turnRight(135);
+        zofka.move(100);
+    }
+
+    private void udelejDomecek(){
+        udelejCtverec();
+        udelejTrojuhelnik();
+    }
+
 
 }
